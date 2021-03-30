2eeda5767a741189b5a23670a72a72cfeeee3dbf
Hunter Kirk <whk@whk.io>
2021-03-29 20:58:06 -0500

    Changed file trigger.sh, Watchman Triggered Commit

--- trigger.sh
+++ trigger.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+
+# Set Email and Author environment variables
+# Also get path to ssh keys and or specify one directly
+
 git add --all
 git commit -am "Changed file $*, Watchman Triggered Commit"
 git push
